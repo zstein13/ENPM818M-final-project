49bf8bcd7bed8bd205badc1ed60a3fc61196a668
Zachary Steinberg <zstein13@umd.edu>
2025-12-06 18:35:18 -0500

    Added record for csv

---
+++ include/record.hpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <sstream>
+
+struct Record
+{
+    std::string email;
+    std::string name;
+    std::string course;
+
+    operator std::string() const {
+        std::stringstream stream;
+
+        stream << email << ", " << name << ", " << course;
+
+        return stream.str();
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,14 @@
 #include "server.hpp"
 #include "code.hpp"
 #include "header.hpp"
+#include "record.hpp"
 
 int main() {
+<<<<<<< Updated upstream
     server();
+=======
+    // server();
+    Record rec{"zstein13@umd.edu", "Zach", "ENPM818M"};
+    std::cout << "Record: " << static_cast<std::string>(rec) << '\n';
+>>>>>>> Stashed changes
 }
